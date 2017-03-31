e0e4a7fee482fd2457f432ed27b3a1e00f73fab1
oftl <github@rockandroll.at>
2017-03-31 14:35:53 +0200

    check returncode only when testing

--- tpen/tests/test_00_command.py
+++ tpen/tests/test_00_command.py
@@ -5,11 +5,11 @@ class TestCommand (unittest.TestCase):
 
     def test_command (self):
         self.assertEqual (
-            run (['ls', './command.py']),
-            {'returncode': 0, 'stdout': b'./command.py\n', 'stderr': b''},
+            run (['ls', './command.py']).get ('returncode'),
+            0,
         )
 
         self.assertEqual (
-            run (['ls', './COMMAND.PY']),
-            {'returncode': 2, 'stdout': b'', 'stderr': b"ls: cannot access './COMMAND.PY': No such file or directory\n"},
+            run (['ls', './COMMAND.PY']).get ('returncode'),
+            2,
         )
