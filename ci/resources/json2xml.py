aadd15bd71aa241147998b2c85c3cfeba204e891
Tara L Andrews <tla@mit.edu>
2017-06-07 14:18:16 +0200

    add our project configuration to json2xml script

--- ci/resources/json2xml.py
+++ ci/resources/json2xml.py
@@ -9,15 +9,62 @@ import argparse
 import tpen2tei.json2xml
 
 
-def metadata():
-    return None
+metadata = {
+    'title': 'Ժամանակագրութիւն',
+    'author': 'Մատթէոս Ուռհայեցի'
+}
 
-def special_chars():
-    return None
+special_chars = {
+    'աշխարհ': ('asxarh', 'ARMENIAN ASHXARH SYMBOL'),
+    'ամենայն': ('amenayn', 'ARMENIAN AMENAYN SYMBOL'),
+    'արեգակն': ('aregakn', 'ARMENIAN AREGAKN SYMBOL'),
+    'լուսին': ('lusin', 'ARMENIAN LUSIN SYMBOL'),
+    'որպէս': ('orpes', 'ARMENIAN ORPES SYMBOL'),
+    'երկիր': ('erkir', 'ARMENIAN ERKIR SYMBOL'),
+    'երկին': ('erkin', 'ARMENIAN ERKIN SYMBOL'),
+    'ընդ': ('und', 'ARMENIAN END SYMBOL'),
+    'ըստ': ('ust', 'ARMENIAN EST SYMBOL'),
+    'պտ': ('ptlig', 'ARMENIAN PEH-TIWN LIGATURE'),
+    'թբ': ('tblig', 'ARMENIAN TO-BEN LIGATURE'),
+    'թե': ('techlig', 'ARMENIAN TO-ECH LIGATURE'),
+    'թի': ('tinilig', 'ARMENIAN TO-INI LIGATURE'),
+    'թէ': ('tehlig', 'ARMENIAN TO-EH LIGATURE'),
+    'էս': ('eslig', 'ARMENIAN EH-SEH LIGATURE'),
+    'ես': ('echslig', 'ARMENIAN ECH-SEH LIGATURE'),
+    'յր': ('yrlig', 'ARMENIAN YI-REH LIGATURE'),
+    'րզ': ('rzlig', 'ARMENIAN REH-ZA LIGATURE'),
+    'զմ': ('zmlig', 'ARMENIAN ZA-MEN LIGATURE'),
+    'թգ': ('tglig', 'ARMENIAN TO-GIM LIGATURE'),
+    'ա': ('avar', 'ARMENIAN AYB VARIANT'),
+    'հ': ('hvar', 'ARMENIAN HO VARIANT'),
+    'յ': ('yabove', 'ARMENIAN YI SUPERSCRIPT VARIANT')    
+}
 
-def numeric_parser():
-    return None
+def numeric_parser(val):
+    """Given the text content of a <num> element, try to turn it into a number."""
+    # Create the stack of characters
+    sigfigs = [ord(c) for c in val.replace('և', '').upper() if ord(c) > 1328 and ord(c) < 1365]
+    total = 0
+    last = None
+    for ch in sigfigs:
+        # What is this one's numeric value?
+        if ch < 1338:    # Ա-Թ
+            chval = ch - 1328
+        elif ch < 1347:  # Ժ-Ղ
+            chval = (ch - 1337) * 10
+        elif ch < 1356:  # Ճ-Ջ
+            chval = (ch - 1346) * 100
+        else:            # Ռ-Ք
+            chval = (ch - 1355) * 1000
 
+        # Put it in the total
+        if last is None or chval < last:
+            total += chval
+        else:
+            total *= chval
+        last = chval
+    return total
+    
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
@@ -47,7 +94,7 @@ if __name__ == '__main__':
         indir               = args.indir,
         outdir              = args.outdir,
         write_stdout_stderr = args.write_stdout_stderr,
-        metadata            = metadata(),
-        special_chars       = special_chars(),
-        numeric_parser      = numeric_parser(),
+        metadata            = metadata,
+        special_chars       = special_chars,
+        numeric_parser      = numeric_parser,
     )
