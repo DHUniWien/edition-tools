f41ec35fbd1eaba1e6d682d5523f88ab596fcbb9
oftl <github@rockandroll.at>
2017-06-07 11:34:30 +0200

    add wrapper script for tpen2tei.json2xml

---
+++ ci/resources/tpen2tei.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+"""
+Wrapper for tpen2tei.json2xml
+"""
+
+import argparse
+
+import tpen2tei.json2xml
+
+
+def metadata():
+    return None
+
+def special_chars():
+    return None
+
+def numeric_parser():
+    return None
+
+if __name__ == '__main__':
+
+    parser = argparse.argumentparser()
+    parser.add_argument (
+         "indir",
+         help = "input directory t-pen output files",
+    )
+    parser.add_argument (
+        "outdir",
+        help = "output directory",
+    )
+    parser.add_argument (
+        "-w",
+        "--write_stdout_stderr",
+        action = "store_true",
+        help = "write stdout and stderr to separate files in outdir",
+    )
+
+    args = parser.parse_args()
+
+    #  logging.basicConfig (
+    #      format = '%(asctime)s %(message)s',
+    #      filename = '%s.log' % os.path.basename (sys.argv[0]),
+    #  )
+
+    json2xml.json2xml (
+        indir               = args.get ('indir'),
+        outdir              = args.get ('outdir'),
+        write_stdout_stderr = args.get ('write_stdout_stderr'),
+        metadata            = metadata(),
+        special_chars       = special_chars(),
+        numeric_parser      = numeric_parser(),
+    )
